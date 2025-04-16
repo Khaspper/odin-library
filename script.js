89ae8336240b603be58a49639734b5d536d8458a
Mark <brandonnarciso24@gmail.com>
2025-04-16 16:32:19 -0700

    Add comments

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const myLibrary = [];
 
-function Book(name, author, numPages, read, uniqueID) {
+function Book(name, author, numPages, read) {
   this.name = name;
   this.author = author;
   this.numPages = numPages;
   this.read = read;
-  this.uniqueID = uniqueID;
+  this.uniqueID = crypto.randomUUID();
 }
 
 function addBookToLibrary(name, author, numPages, read) {
-  const newBook = new Book(name, author, numPages, read, crypto.randomUUID());
+  const newBook = new Book(name, author, numPages, read);
   myLibrary.push(newBook);
+  renderLibrary();
 }
+
+function renderLibrary() {
+  
+}
+
+addBookToLibrary("The amazing spider-man", "Stan Lee", 200, true);
+addBookToLibrary("Poopy", "Stan Lee", 200, true);
+addBookToLibrary("Gangsters say whaaaaat?", "Stan Lee", 50, false);
