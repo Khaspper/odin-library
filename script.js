4dc77542f1fd73275fa9d45f66b1da699d21b780
Mark <brandonnarciso24@gmail.com>
2025-04-16 16:23:04 -0700

    Add Book constructor, UUID generation
    users can now add books to the array

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const myLibrary = [];
 
-function Book() {
-
+function Book(name, author, numPages, read, uniqueID) {
+  this.name = name;
+  this.author = author;
+  this.numPages = numPages;
+  this.read = read;
+  this.uniqueID = uniqueID;
 }
 
-function addBookToLibrary() {
-
+function addBookToLibrary(name, author, numPages, read) {
+  const newBook = new Book(name, author, numPages, read, crypto.randomUUID());
+  myLibrary.push(newBook);
 }
